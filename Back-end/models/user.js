0a437a7d5b39483a27dee1eede9a5fe7dccad0cd
Aya Anwar <135036578+aya-cs22@users.noreply.github.com>
2025-02-10 00:36:24 +0200

    Add backend model user for e-commerce

--- Back-end/models/user.js
+++ Back-end/models/user.js
@@ -12,10 +12,8 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Email is required'],
         unique:true,
         lowercase: true,
-        match: [
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        'Please enter a valid email'
-        ]
+        validate: [validator.isEmail, 'Please provide a valid email'],
+
     },
     password: {
         type: String,
